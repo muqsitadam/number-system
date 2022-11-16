0cf455106abbc6e055467d3f34cf1d3cec4e1b74
muqsitadam <muqsitadam@gmail.com>
2022-11-16 19:43:01 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -22,9 +22,9 @@ class App extends Component{
       <div>
         <h1 className='text-3xl text-center mt-10'>Number System</h1>
         <div className="text-2xl flex sm:flex-row flex-col-reverse justify-center items-center bg-neutral-200 w-full lg:w-1/2 2xl:w-1/3 m-auto mt-10 rounded-xl">
-        <button className='hover:text-3xl p-5 m-10 rounded-xl text-white bg-red-700 font-mono' onClick={this.decrementNumber}>Decrement</button>
+        <button className='hover:text-3xl p-5 m-10 rounded-xl text-white bg-red-700 font-mono' onClick={this.decrementNumber}>Reduce count</button>
         <p className='px-10 py-5 border-2 rounded-xl border-black'>{this.state.number}</p>
-        <button className='hover:text-3xl p-5 m-10 rounded-xl text-white bg-green-700 font-mono' onClick={this.incrementNumber}>Increment</button>
+        <button className='hover:text-3xl p-5 m-10 rounded-xl text-white bg-green-700 font-mono' onClick={this.incrementNumber}>Increase count</button>
         </div>
       </div>
       
