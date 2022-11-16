2cae1a5f8556040a641fcc2c480abcbc50a6f229
muqsitadam <muqsitadam@gmail.com>
2022-11-16 19:16:55 +0100

    Updated the buttons

--- src/App.js
+++ src/App.js
@@ -21,10 +21,10 @@ class App extends Component{
     return (
       <div>
         <h1 className='text-3xl text-center mt-10'>Number System</h1>
-        <div className="text-2xl flex flex-row justify-center items-center bg-neutral-200 w-1/2 m-auto mt-10 rounded-xl">
-        <button className='hover:text-3xl p-10 m-10 font-mono' onClick={this.decrementNumber}>Decrement</button>
+        <div className="text-2xl flex flex-row justify-center items-center bg-neutral-200 w-1/2 m-auto mt-10 rounded-xl h-60">
+        <button className='hover:text-3xl p-5 m-10 rounded-xl text-white bg-red-700 font-mono' onClick={this.decrementNumber}>Decrement</button>
         <p className='px-10 py-5 border-2 rounded-xl border-black'>{this.state.number}</p>
-        <button className='hover:text-3xl p-10 m-10 font-mono' onClick={this.incrementNumber}>Increment</button>
+        <button className='hover:text-3xl p-5 m-10 rounded-xl text-white bg-green-700 font-mono' onClick={this.incrementNumber}>Increment</button>
         </div>
       </div>
       
